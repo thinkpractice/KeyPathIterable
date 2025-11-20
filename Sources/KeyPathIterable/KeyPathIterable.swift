8ea1b641b6dd7bbffad9deaac5b06c170eb7ff84
Tim De Jong <gitlab@thinkpractice.nl>
2025-11-20 17:17:59 +0100

    Added tensorflow functions in a public extension

--- Sources/KeyPathIterable/KeyPathIterable.swift
+++ Sources/KeyPathIterable/KeyPathIterable.swift
@@ -46,3 +46,51 @@ public extension KeyPathIterable {
         []
     }
 }
+
+
+// Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exceptions.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+public extension KeyPathIterable {
+  /// Returns an array of all custom key paths of this value, to the specified
+  /// type.
+  func allKeyPaths<T>(to _: T.Type) -> [KeyPath<Self, T>] {
+    return allKeyPaths.compactMap { $0 as? KeyPath<Self, T> }
+  }
+
+  /// Returns an array of all custom key paths of this value and any custom key
+  /// paths nested within each of what this value's key paths refers to, to
+  /// the specified type.
+  func recursivelyAllKeyPaths<T>(to _: T.Type) -> [KeyPath<Self, T>] {
+    return recursivelyAllKeyPaths.compactMap { $0 as? KeyPath<Self, T> }
+  }
+
+  /// Returns an array of all custom writable key paths of this value, to the
+  /// specified type.
+  func allWritableKeyPaths<T>(to _: T.Type) -> [WritableKeyPath<Self, T>] {
+    return allKeyPaths(to: T.self)
+      .compactMap { $0 as? WritableKeyPath<Self, T> }
+  }
+
+  /// Returns an array of all custom writable key paths of this value and any
+  /// custom writable key paths nested within each of what this value's key
+  /// paths refers to, to the specified type.
+  func recursivelyAllWritableKeyPaths<T>(
+    to _: T.Type
+  ) -> [WritableKeyPath<Self, T>] {
+    return recursivelyAllKeyPaths(to: T.self)
+      .compactMap { $0 as? WritableKeyPath<Self, T> }
+  }
+}
